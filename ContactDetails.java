dc4973229681001021ff2f172303faee1455d4de
DevikaRajendran-dev <134774292+DevikaRajendran-dev@users.noreply.github.com>
2024-11-19 23:26:27 +0530

    Update ContactDetails.java

--- ContactDetails.java
+++ ContactDetails.java
@@ -2,7 +2,7 @@ package junitinterface;
 
 public interface ContactDetails {
     boolean addContact(String fName, String lName, String number);
-    boolean deleteContact(String firstName);
+    boolean deleteContact(String fName);
     boolean updateContact(String fName, String number);
     boolean viewContact(String fName);
 }
